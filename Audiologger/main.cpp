f82f7f47eb60d94af17c27f5adf7531095b45f48
Malte Poll <malte.poll@web.de>
2016-02-15 02:28:19 +0100

    Initial Commit

---
+++ Audiologger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Audiologger
+//
+//  Created by Malte Poll on 15.02.16.
+//  Copyright © 2016 Malte Poll. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
